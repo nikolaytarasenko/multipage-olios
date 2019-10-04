8f18c2ea63fd6effd74da7ba17f88b94870875f7
Nikolay Tarasenko <kolyatarasenko@ukr.net>
2019-10-04 11:53:05 +0300

    product page was added

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -16,4 +16,34 @@
             menu.classList.remove('js-show');
         }
     }
+
+    // zoom image on the product page
+    const zoomSizes = [
+        'scale(0.7)',
+        'scale(0.8)',
+        'scale(0.9)',
+        'scale(1)',
+        'scale(1.1)',
+        'scale(1.2)',
+        'scale(1.3)'
+    ];
+    const productBigImage = document.querySelector('.product__img');
+    const zoomIn = document.querySelector('.zoom__plus');
+    const zoomOut = document.querySelector('.zoom__minus');
+
+    productBigImage.style.transform = zoomSizes[Math.floor(zoomSizes.length / 2)];
+
+    zoomIn.addEventListener('click', function(e) {
+        const currentZoom = productBigImage.style.transform;
+        const currentZoomIndex = zoomSizes.indexOf(currentZoom);
+
+        return (zoomSizes[currentZoomIndex + 1]) ? productBigImage.style.transform = zoomSizes[currentZoomIndex + 1] : false;
+    });
+
+    zoomOut.addEventListener('click', function(e) {
+        const currentZoom = productBigImage.style.transform;
+        const currentZoomIndex = zoomSizes.indexOf(currentZoom);
+
+        return (zoomSizes[currentZoomIndex - 1]) ? productBigImage.style.transform = zoomSizes[currentZoomIndex - 1] : false;
+    });
 })();
